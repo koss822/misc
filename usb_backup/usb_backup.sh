fe0f2b06d992953edad29dc875813aeff108f913
enigma14 <mkonicek12@gmail.com>
2015-06-19 10:49:54 +0200

    added function to delete old backups if not enough space

--- usb_backup/usb_backup.sh
+++ usb_backup/usb_backup.sh
@@ -22,14 +22,43 @@ KEYFILE=/root/keyfile
 
 EXCLUDE=exclude.txt
 
+# Clean free space and backup
+function backup_and_clean() {
+    while true; do
+        rsync --exclude-from=$EXCLUDE -av --del --ignore-errors $BKP_SOURCE/* \
+            $BKP_TARGET/current/ 2>&1 | tee /tmp/rsync.log
+        if ! grep -qi "No space left" /tmp/rsync.log; then
+            break
+        fi
+        # remove old backup
+        oldest=$(sudo btrfs subvolume list -a $BKP_TARGET/ | grep snaps \
+            | head -n1 | awk '{ print $9 }' | cut -d/ -f2)
+        # check we are not removing last backup
+        if [ "$backups" = "$taken" ]; then
+            read -n1 -r -p  \
+                "Do you want to delete last backup, there is no space on device... [y/n]" key
+            if [ ! "$key" = "y" ]; then
+                echo "No space left, exiting..."
+                exit 1
+            fi
+        fi
+        echo "Removing $oldest"
+        if ! sudo btrfs subvolume delete $BKP_TARGET/snaps/$oldest; then
+            echo "Cannot remove $BKP_TARGET/snaps/$oldest, exiting..."
+            exit 1
+        fi
+    done
+}
+
 case "$1" in
     backup)
+        taken=`cat $BKP_TARGET/taken`
         if [ -f $BKP_TARGET/taken ]; then
             mkdir -p $BKP_TARGET/snaps
-            sudo btrfs subvolume snapshot $BKP_TARGET/current $BKP_TARGET/snaps/`cat $BKP_TARGET/taken`
+            sudo btrfs subvolume snapshot $BKP_TARGET/current $BKP_TARGET/snaps/$taken
         fi
         date +%Y-%m-%d_%H:%M > $BKP_TARGET/taken
-        rsync --exclude-from=$EXCLUDE -av --del --ignore-errors $BKP_SOURCE/* $BKP_TARGET/current/
+        backup_and_clean
         ;;
     mount)
         sudo cryptsetup luksOpen $2 backups --key-file $KEYFILE
