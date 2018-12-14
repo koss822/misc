d523b6c2d59ebc7cf39c65747c4430099ff0cf73
koss822 <mkonicek12@gmail.com>
2018-12-15 00:23:58 +0100

    CURL fix and enable ssh

--- rpiwhitenoise/install.sh
+++ rpiwhitenoise/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# EXEC - curl http://bit.ly/rpiwhitenoise | sudo bash
+# EXEC - curl -L http://bit.ly/rpiwhitenoise | sudo bash
 apt install mpg123
 systemctl enable autologin@tty1.service
+systemctl start ssh
+systemctl enable ssh
 wget https://s3-eu-west-1.amazonaws.com/koss-public-misc/github/fan.mp3
 wget https://raw.githubusercontent.com/koss822/misc/master/rpiwhitenoise/fan.sh
 chmod +x fan.sh
