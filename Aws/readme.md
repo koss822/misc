132dd63b4c417a2d065ab437e52fe20fb3c6a86f
koss822 <mkonicek12@gmail.com>
2019-09-24 20:01:08 +0200

    Cloudformation template for website check

--- Aws/readme.md
+++ Aws/readme.md
@@ -1,12 +1,15 @@
 # AWS scripts and tools
 
-## [HTTP HealthCheck Reboot Tool](https://github.com/koss822/misc/blob/master/Aws/http_healthcheck_reboot_lambda.py)
+## [HTTP HealthCheck Reboot Tool](https://github.com/koss822/misc/blob/master/Aws/website_check/)
 
 This is a very simple lambda script for rebooting EC2 instance when HTTP(s) site is unreachable or does not contain selected text.
 
 When you create this Lambda script in AWS you should trigger it by _CloudWatch Events_ trigger with _Schedule expression_ set up for _rate(1 hour)_ or whatever you like. For most scenarios it should run completely free of charge in free tier usage.
 
 It is also needed to increase timeout for this script - recommended value is 10 minutes but might be lower.
+
+**NEW 2019**
+I have created a CloudFormation template for you with prepublicated Lambda source code in S3 bucket. Everything what you need to do is deploy cloudformation template with Parameters (SNS topics which you create yourself for alerting, instance ID, region) and you are ready!
 
 ### Diagram
 ![HTTP Health Check](https://raw.githubusercontent.com/koss822/misc/master/imgs/http_health_check.png "HTTP Health Check diagram")
--- Aws/website_check/lambda_function.py
+++ Aws/website_check/lambda_function.py
@@ -1,15 +1,14 @@
-# See https://www.enigma14.eu/martin/blog/2018/10/24/aws-lambda-reboot-instance-when-http-unreachable-for-free/
-
 import json
 from botocore.vendored import requests
 import boto3
 import time
-# e.g. eu-west-1
-region = 'xx-xxxx-x'
-instanceID = 'x-xxxxxxxxxxx'
-website = 'https://www.enigma14.eu/'
-webstring = 'SearchText'
-arn = 'arn:aws:sns:eu-west-1:xxxx:xxxx'
+import os
+
+region = os.environ.get('region')
+instanceID = os.environ.get('instanceID')
+website = os.environ.get('website')
+webstring = os.environ.get('webstring')
+snsarn = os.environ.get('snsarn')
 
 def lambda_handler(event, context):
     for i in range(0,3):
@@ -18,14 +17,15 @@ def lambda_handler(event, context):
         time.sleep(60)
     reboot_instance()
     return 'Restarted instances'
-
-
+        
+    
 def check_website():
     r = requests.get(website)
     if webstring in r.text:
         return True
     else:
         return False
+
 def reboot_instance():
     ec2 = boto3.resource('ec2')
     instance = ec2.Instance(instanceID)
@@ -39,4 +39,4 @@ def reboot_instance():
         time.sleep(1)
     instance.start()
     client = boto3.client('sns')
-    client.publish(TopicArn=arn, Message='EC2 instance rebooted!')
+    client.publish(TopicArn=snsarn, Message='EC2 instance rebooted!')
