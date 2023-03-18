40bb7d08e0e9de2bffba1be2bb95e78cb523ecca
mkonicek <5047996+koss822@users.noreply.github.com>
2023-03-18 22:10:04 +0100

    launch template

--- Kubernetes/training/userdata.sh
+++ Kubernetes/training/userdata.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Do not forget to rm -rf /var/lib/cloud
+export HOME="/home/ubuntu/"
 cd /home/ubuntu/misc/Kubernetes/training/
+git config --global --add safe.directory /home/ubuntu/misc
 git pull
+sleep 120
 bash kubeinit.sh
