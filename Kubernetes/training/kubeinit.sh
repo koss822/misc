1b97bbcc202af75085cdc3994a809ff6ef8e5a00
mkonicek <5047996+koss822@users.noreply.github.com>
2023-03-18 21:26:45 +0100

    fixing path

--- Kubernetes/training/kubeinit.sh
+++ Kubernetes/training/kubeinit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown ubuntu:ubuntu -R /home/ubuntu
 sudo snap remove microk8s
 sudo lvremove -y /dev/kube-data/*
 sudo snap install microk8s --classic
@@ -12,8 +13,8 @@ microk8s helm repo add openebs-lvmlocalpv https://openebs.github.io/lvm-localpv
 microk8s helm repo add bitnami https://charts.bitnami.com/bitnami
 microk8s helm delete lvm-localpv
 microk8s helm delete wordpress
-~/misc/Kubernetes/training/init_data_volume.sh
-~/misc/Kubernetes/training/init_openebs.sh
-~/misc/Kubernetes/training/init_sc.sh
-~/misc/Kubernetes/training/init_traefik.sh
-~/misc/Kubernetes/training/init_wordpress_helm.sh
+/home/ubuntu/misc/Kubernetes/training/init_data_volume.sh
+/home/ubuntu/misc/Kubernetes/training/init_openebs.sh
+/home/ubuntu/misc/Kubernetes/training/init_sc.sh
+/home/ubuntu/misc/Kubernetes/training/init_traefik.sh
+/home/ubuntu/misc/Kubernetes/training/init_wordpress_helm.sh
---
+++ Kubernetes/training/userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Do not forget to rm -rf /var/lib/cloud
+cd /home/ubuntu/misc/Kubernetes/training/
+git pull
+bash kubeinit.sh
