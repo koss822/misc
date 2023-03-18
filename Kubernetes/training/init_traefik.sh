f7b667f74fe3c452282d24e40eaa2a9a53db1769
mkonicek <5047996+koss822@users.noreply.github.com>
2023-03-18 22:27:46 +0100

    traefik

--- Kubernetes/training/init_traefik.sh
+++ Kubernetes/training/init_traefik.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo snap install yq
-microk8s kubectl get svc -n traefik traefik -o yaml > ~/traefik.yml
-yq eval '.spec.ports[0].nodePort = 80' ~/traefik.yml -i
-microk8s kubectl apply -f ~/traefik.yml
-rm ~/traefik.yml
+microk8s kubectl get svc -n traefik traefik -o yaml > /root/traefik.yml
+yq eval '.spec.ports[0].nodePort = 80' /root/traefik.yml -i
+microk8s kubectl apply -f /root/traefik.yml
+rm /root/traefik.yml
