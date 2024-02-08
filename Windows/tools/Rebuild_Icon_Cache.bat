f74ee25ca36130f5ce8808dffc50921e883bc4a8
mkonicek <5047996+koss822@users.noreply.github.com>
2024-02-08 10:50:37 +0100

    rebuild icon cache

---
+++ Windows/tools/Rebuild_Icon_Cache.bat
@@ -0,0 +1,57 @@
+:: Created by: Shawn Brink
+:: Created on: April 27th 2015
+:: Updated on: August 30, 2023
+:: Tutorial: https://www.tenforums.com/tutorials/5645-rebuild-icon-cache-windows-10-a.html
+
+
+@echo off
+set iconcache=%localappdata%\IconCache.db
+set iconcache_x=%localappdata%\Microsoft\Windows\Explorer\iconcache*
+
+echo.
+echo The explorer process must be temporarily killed before deleting the IconCache.db file. 
+echo.
+echo Please SAVE ALL OPEN WORK before continuing.
+echo.
+pause
+echo.
+If exist "%iconcache%" goto delete
+echo.
+echo The %localappdata%\IconCache.db file has already been deleted.
+echo. 
+If exist "%iconcache_x%" goto delete
+echo.
+echo The %localappdata%\Microsoft\Windows\Explorer\IconCache_*.db files have already been deleted.
+echo.
+exit /B
+
+
+:delete
+echo.
+echo Attempting to delete IconCache.db files...
+echo.
+ie4uinit.exe -show
+taskkill /IM explorer.exe /F 
+If exist del /A /F /Q "%iconcache%"
+If exist del /A /F /Q "%iconcache_x%"
+del /F /S /Q "%localappdata%\Packages\Microsoft.Windows.Search_cw5n1h2txyewy\localstate\AppIconCache\*.*"
+start explorer.exe
+echo.
+echo IconCache database files have been successfully deleted.
+goto restart
+
+
+:restart
+echo.
+echo.
+echo You will need to restart the PC to finish rebuilding your icon cache.
+echo.
+CHOICE /C:YN /M "Do you want to restart the PC now?"
+IF ERRORLEVEL 2 goto no
+IF ERRORLEVEL 1 goto yes
+
+:yes
+shutdown /r /f /t 00
+
+:no
+exit /B
