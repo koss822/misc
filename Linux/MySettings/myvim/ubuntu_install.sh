5a86419b470f634b1b4f7d74ad1940399f7b346d
Martin Konicek <5047996+koss822@users.noreply.github.com>
2026-01-08 18:27:37 +0100

    ubuntu_install

--- Linux/MySettings/myvim/ubuntu_install.sh
+++ Linux/MySettings/myvim/ubuntu_install.sh
@@ -3,8 +3,10 @@ cd /tmp
 git clone https://github.com/koss822/misc.git
 cd misc
 cp -R Linux/MySettings/myvim/* /etc
+mv /etc/vim/vimrc /etc/vim/vimrc.old
 ln -s /etc/vimrc /etc/vim/vimrc
-mv /usr/share/vim/vimrc /usr/share/vim/vimrc.old
+mkdir -p /usr/share/vim/
+mv /usr/share/vim/vimrc /usr/share/vim/vimrc.old 2>/dev/null
 ln -s /etc/vimrc /usr/share/vim/vimrc
 rm -rf /tmp/misc
 update-alternatives --set editor /usr/bin/vim.basic
