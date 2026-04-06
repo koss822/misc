5e86d080d0ae3ed8e2c5ab6d650f3096be43771f
Martin Konicek <5047996+koss822@users.noreply.github.com>
2026-04-06 11:30:30 +0200

    fix

--- Linux/Ubuntu-VNC/setup-lxqt-vnc.sh
+++ Linux/Ubuntu-VNC/setup-lxqt-vnc.sh
@@ -267,7 +267,7 @@ After=network.target
 Type=forking
 Environment=HOME=${CURRENT_HOME}
 ExecStartPre=/bin/sh -c "/usr/bin/vncserver -kill :1 > /dev/null 2>&1 || :"
-ExecStart=/usr/bin/vncserver :1 -geometry ${VNC_RES} -depth 24 -localhost yes
+ExecStart=/usr/bin/vncserver :1 -geometry ${VNC_RES} -depth 24 -localhost yes -rfbauth ${CURRENT_HOME}/.config/tigervnc/passwd
 ExecStop=/usr/bin/vncserver -kill :1
 Restart=on-failure
 RestartSec=5
